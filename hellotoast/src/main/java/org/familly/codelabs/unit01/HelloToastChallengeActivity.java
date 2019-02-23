e7e45cd7d5f0a643a3e70dd83b92b2301182ccfb
sinaburokik8725 <sinaburokim8725@gmail.com>
2019-02-23 21:29:55 +0900

    구글개발사에서제굥하는코드랩 실습 Initial commit

---
+++ hellotoast/src/main/java/org/familly/codelabs/unit01/HelloToastChallengeActivity.java
@@ -0,0 +1,39 @@
+package org.familly.codelabs.unit01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HelloToastChallengeActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView txtCount;
+
+    private String DEBUG_TAG = "CODE_LABS";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_toast_challenge);
+        txtCount = findViewById(R.id.text_count_challenge);
+    }
+    //xml에서 버튼 속성으로 onClick 이벤트를 잡는 방식이다.
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        Log.d(DEBUG_TAG,"START countUp()");
+        mCount++;
+        if (txtCount != null) {
+            txtCount.setText(Integer.toString(mCount));
+        }
+
+        Log.d(DEBUG_TAG,"END countUp()");
+
+    }
+}
